c6fb9411601e6276d4fa348486f2f9a950b97366
Marlène BOTON <sessi>
2019-05-01 19:44:51 +0200

    article-creation

---
+++ simple-app/src/app/article-creation/article-creation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article-creation',
+  templateUrl: './article-creation.component.html',
+  styleUrls: ['./article-creation.component.css']
+})
+export class ArticleCreationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
